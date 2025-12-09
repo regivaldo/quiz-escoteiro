04aa408910f1c457d1f1a13874f53666ff55e9e0
Regivaldo Silva <regivaldorfs@gmail.com>
2025-12-09 09:38:17 -0300

    feat: integrate React Query by wrapping `PrivateTemplate` and `AuthTemplate` with `QueryClientProvider`

--- src/modules/auth/AuthTemplate.tsx
+++ src/modules/auth/AuthTemplate.tsx
@@ -1,15 +1,20 @@
 import { Outlet } from 'react-router'
 import Page from './components/Page'
 import Header from './components/Header'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const AuthTemplate = () => {
+    const queryClient = new QueryClient();
+
     return (
-        <main className="bg-bg-light font-display text-text-on-light min-h-screen">
-            <Page>
-                <Header />
-                <Outlet />
-            </Page>
-        </main>
+        <QueryClientProvider client={queryClient}>
+            <main className="bg-bg-light font-display text-text-on-light min-h-screen">
+                <Page>
+                    <Header />
+                    <Outlet />
+                </Page>
+            </main>
+        </QueryClientProvider>
     )
 }
 
--- src/modules/private/PrivateTemplate.tsx
+++ src/modules/private/PrivateTemplate.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router'
 import PrivateHeader from '@/modules/private/components/PrivateHeader'
 import PrivateFooter from '@/modules/private/components/PrivateFooter'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const PrivateTemplate = () => {
+    const queryClient = new QueryClient();
+
     return (
-        <div className="relative flex min-h-screen w-full flex-col bg-background font-display text-text">
-            <PrivateHeader />
-            <Outlet />
-            <PrivateFooter />
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className="relative flex min-h-screen w-full flex-col bg-background font-display text-text">
+                <PrivateHeader />
+                <Outlet />
+                <PrivateFooter />
+            </div>
+        </QueryClientProvider>
     )
 }
 
